f17d87a56846af996a0dff2e6aca2cd2ee8ba0e1
Bruno Fernando Feitoza <bfeitoza634cyt@gmail.com>
2021-03-27 15:00:08 -0300

    feat: integrate with stripe using ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
-import { SubscribeButton } from "components/SubscribeButton";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+
+import { SubscribeButton } from "components/SubscribeButton";
+import { stripe } from "services/stripe";
 
 import styles from "styles/pages/home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,10 +30,10 @@ export default function Home() {
 
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
@@ -31,3 +41,22 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IZfQnJBDweHRUz5eFr9cv05");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
