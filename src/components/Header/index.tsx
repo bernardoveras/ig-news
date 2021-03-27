984f6c99579026f9764ded8a02dc60cb145b0485
Bruno Fernando Feitoza <bfeitoza634cyt@gmail.com>
2021-03-27 13:41:47 -0300

    feat: home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from "components/SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -6,9 +7,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a href="#" className={styles.active}>Home</a>
+          <a href="#" className={styles.active}>
+            Home
+          </a>
           <a href="#">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
