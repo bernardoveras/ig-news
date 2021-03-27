61e9346ecbe7438d50c9bee129b8a33a4997739c
Bruno Fernando Feitoza <bfeitoza634cyt@gmail.com>
2021-03-27 16:30:07 -0300

    feat: oauth with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,26 @@
+import { signIn, signOut, useSession } from "next-auth/client";
 import { FaGithub } from "react-icons/fa";
-import { FiX } from 'react-icons/fi';
+import { FiX } from "react-icons/fi";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const [session] = useSession();
+
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Bruno Fernando Feitoza
+      {session.user.name}
       <FiX color="#737380" />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
-  )
+  );
 }
